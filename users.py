df98b016ffd8e27afe4500ae6cb1e889bd5e918d
Brayan7546 <99565885+Brayan7546@users.noreply.github.com>
2022-02-23 23:12:53 -0500

    Add files via upload

---
+++ users.py
@@ -0,0 +1,7 @@
+users =[
+    {"nombre":"Marcela Castro","telefono":666777,"edad":20},
+    {"nombre":"Carlos Paez","telefono":727783,"edad":17},
+    {"nombre":"Carolina Antequera","telefono":123717,"edad":33},
+    {"nombre":"Daniela Vaquero","telefono":883716,"edad":18},
+    {"nombre":"Javier Camacho","telefono":837265,"edad":40},
+]
